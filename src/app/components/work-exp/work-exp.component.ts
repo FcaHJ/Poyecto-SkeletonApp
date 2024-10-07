b2be04f45ca19e8751471d98d2eb1167ddfdd001
FcaHJ <fran.herrera@duocuc.cl>
2024-10-07 20:08:18 -0300

    Modificacion componentes

--- src/app/components/work-exp/work-exp.component.ts
+++ src/app/components/work-exp/work-exp.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-work-exp',
@@ -7,8 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExpComponent  implements OnInit {
 
-  constructor() { }
+  username: string = 'guest';
+  name: string = '';
+  lastname: string = '';
+  educationLevel: string = '';
+  bday: string = '';
+  alertButtons = ['Close'];
+
+  educationLevels: Map<string, string> = new Map<string, string>();
+
+  constructor(private router: Router, private alertController: AlertController) {
+    const state = this.router.getCurrentNavigation()?.extras?.state;
+    if (state) {
+      this.username = state['user'];
+    }
+
+    this.educationLevels.set("pre","Pre Escolar");
+    this.educationLevels.set("basica","Ed. Basica");
+    this.educationLevels.set("media","Ed. Media");
+    this.educationLevels.set("superior","Ed. Superior");
+    this.educationLevels.set("postgrado","Postgrado");
+    this.educationLevels.set("doctorado","doctorado");
+   }
 
   ngOnInit() {}
+
+  async showInfo(){
+    const alert = await this.alertController.create({
+      header: 'Usuario',
+      subHeader: '[controller]',
+      message: `El nombre de usuario es ${this.name} ${this.lastname}`,
+      buttons: this.alertButtons
+    });
+    alert.present();
+  }
+
+  clean(){
+    this.name = '',
+    this.lastname = '',
+    this.educationLevel = '',
+    this.bday = ''
+  }
 
 }
